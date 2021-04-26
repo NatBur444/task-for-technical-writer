343f0d9f7cb069fdb930a2f5bb35a7fb82c3e3b4
NatBur444 <natalya.buravova@gmail.com>
2021-04-26 11:37:30 +0500

    Update Position.cs

--- Models/Position.cs
+++ Models/Position.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json;
 
-namespace Kontur.Employee
+namespace Employee
 {
     public class Position
     {
@@ -24,25 +24,6 @@ namespace Kontur.Employee
         public override string ToString()
         {
             return $"O{Organization}<->P{Person}: {Title}";
-        }
-
-        public override int GetHashCode()
-        {
-            return Person.GetHashCode() * Organization.GetHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (this == obj)
-                return true;
-
-            var other = obj as Position;
-            if (other == null)
-                return false;
-
-            return Person == other.Person
-                   && Organization == other.Organization
-                   && Title == other.Title;
         }
     }
 }
